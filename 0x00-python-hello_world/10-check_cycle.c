81fdc837caf10d41ffffa9551bde3fbc0f314496
Gurnelius <terergurnelius@gmail.com>
2023-11-27 19:39:56 +0300

    Initial commit

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * check_cycle - check if list is empty
+ * @list: pointer to list to be checked
+ * Return: 0 - empty, 1 otherwise
+ */
+int check_cycle(listint_t *list)
+{
+	if (list == NULL)
+	{
+		return (0);
+	}
+	else
+		return (1);
+}
