6a80965153f2505eb0496872144417a69750d3d5
Gurnelius <terergurnelius@gmail.com>
2023-11-28 23:18:15 +0300

    Initial commit

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -5,6 +5,6 @@ for a in range(10):
         if a == b or a > b:
             continue
         if (a < 9 and b < 9):
-            print(f"{a}{b}, ", end='')
+            print("{}{}".format(a,b), end=', ')
         elif a == 8 and b == 9:
-            print(f"{a}{b}")
+            print("{}{}".format(a, b))
