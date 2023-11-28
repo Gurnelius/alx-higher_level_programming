caabddb69db101892daeeed61f7ed0d13b0afd66
Gurnelius <terergurnelius@gmail.com>
2023-11-28 21:22:24 +0300

    Betty code style

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -19,16 +19,16 @@ listint_t *insert_node(listint_t **head, int number)
 
 	if (new_node == NULL)
 		return (NULL);
-	
+
 	new_node->n = number;
 	new_node->next = NULL;
 
-	if (*head == NULL)         
-        {
-                *head = new_node;        
-                return (*head);   
-        }
-	
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (*head);
+	}
+
 	current = *head;
 	next = *head;
 	while (next->next != NULL)
