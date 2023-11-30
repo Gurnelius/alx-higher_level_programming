e078959d4d58248a93038186cd9362c24a5821a1
Gurnelius <terergurnelius@gmail.com>
2023-11-30 22:19:42 +0300

    Update code

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,12 +1,2 @@
 #!/usr/bin/python3
-import os
-
-if __name__ == "__main__":
-    stdout_fd = 1
-    text = "#pythoniscool\n"
-
-    stdout_file = os.fdopen(stdout_fd, 'w')
-
-    stdout_file.write(text)
-
-    stdout_file.close()
+__import__("os").write(1, "#pythoniscool\n".encode("UTF-8"))
