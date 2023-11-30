5b7d76611ec9fe411befa7a9c3e900e105c48a80
Gurnelius <terergurnelius@gmail.com>
2023-11-30 18:54:26 +0300

    Update code

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,7 @@
 
 if __name__ == "__main__":
     from sys import argv
-    print(f"{len(argv) - 1} arguments")
+    print("{} arguments".format(len(argv) - 1))
 
     if len(argv) > 1:
         for i in range(1, len(argv)):
