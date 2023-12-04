97b71bd5c99d459e80caa8d7d1867ff21c911421
Gurnelius <terergurnelius@gmail.com>
2023-12-04 13:21:05 +0300

    Initial commit

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,4 +3,8 @@
 def max_integer(my_list[]):
     if len(my_list) == 0:
         return None
-    return (max(my_list))
+    max_n = 0
+    for i in my_list:
+        if max_n < i:
+            max_n = i
+    return max_n
