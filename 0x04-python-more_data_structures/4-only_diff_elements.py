54e46d29831508d90a6e0e5083e59209b15546c7
Gurnelius <terergurnelius@gmail.com>
2023-12-06 15:17:04 +0300

    Update code

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 
-def common_elements(set_1, set_2):
-    return [x for x in set_1 if x not in set_2]
+def only_diff_elements(set_1, set_2):
+    new_set = set()
+    [new_set.add(x) for x in set_1 if x not in set_2]
+    [new_set.add(x) for x in set_2 if x not in set_1]
+    return new_set
