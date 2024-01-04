5ac96b3f742fe29128367b79211a3e4e7113ea96
Gurnelius <terergurnelius@gmail.com>
2024-01-04 20:49:48 +0300

    Add error handling

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -41,7 +41,18 @@ class Rectangle:
         - height (int): The height of the rectangle (default is 0).
         """
         Rectangle.number_of_instances += 1
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+
         self.__width = width
+
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+
         self.__height = height
 
     @property
