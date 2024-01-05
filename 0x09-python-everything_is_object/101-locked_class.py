11e2312bfaf4811bc85ddbaf669156b698877752
Gurnelius <terergurnelius@gmail.com>
2024-01-05 21:59:33 +0300

    Initial commit

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -11,9 +11,3 @@ class LockedClass:
     except if the new instance attribute is called first_name.
     '''
     __slots__ = ('first_name',)
-
-    def __setattr__(self, name, value):
-        if name != 'first_name':
-            err_msg = f"'{self.name}' object has no attribute '{name}'"
-            raise AttributeError(err_msg)
-        super().__setattr__(name, value)
