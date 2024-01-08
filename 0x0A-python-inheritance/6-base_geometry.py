1a2f6670f86a706a2ef787213b6dbed5ccaf17cf
Gurnelius <terergurnelius@gmail.com>
2024-01-08 14:35:10 +0300

    Initial commit

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''
+    Module to implement BaseGeometry class
+'''
+
+
+class BaseGeometry:
+    '''
+        BaseGeometry class
+        Methods:
+            -area: not implemented
+    '''
+    def area(self):
+        '''
+            Area method.
+            Parameters:
+                -None
+        '''
+        raise Exception("area() is not implemented")
