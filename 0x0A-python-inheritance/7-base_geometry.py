a938de739a94252da3a90d202cc0f4089c5c3191
Gurnelius <terergurnelius@gmail.com>
2024-01-08 21:21:55 +0300

    Fix minor issue

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -34,6 +34,6 @@ class BaseGeometry:
             than or equal to 0.
         """
         if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer")
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+            raise ValueError("{} must be greater than 0".format(name))
