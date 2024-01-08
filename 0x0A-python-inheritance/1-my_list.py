dc166f5dbd75088ed80ae6befc400dc356db85c8
Gurnelius <terergurnelius@gmail.com>
2024-01-08 21:15:07 +0300

    Initial commit

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -12,21 +12,11 @@ class MyList(list):
     Public instance method:
     - print_sorted(self): Prints the list in
     ascending sorted order.
-
-    Example:
-    >>> my_list = MyList([5, 2, 7, 1, 10])
-    >>> my_list.print_sorted()
-    [1, 2, 5, 7, 10]
     """
 
     def print_sorted(self):
         """
         Prints the list in ascending sorted order.
-
-        Example:
-        >>> my_list = MyList([4, 2, 1])
-        >>> my_list.print_sorted()
-        [1, 2, 4]
         """
         sorted_list = sorted(self)
         print(sorted_list)
