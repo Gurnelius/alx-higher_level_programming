c585fba9bf264aff78561a96a5746672287dda11
Gurnelius <terergurnelius@gmail.com>
2024-01-09 21:58:17 +0300

    Initial commit

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 '''
+    Generate dictionary description with simple
+    data structure (list, dictionary, string,
+    integer and boolean)
+    for JSON serialization of an object
 '''
 
 
