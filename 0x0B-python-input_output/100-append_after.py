2367deab55cf4ab7aaf7a4dd8642cbf595b06711
Gurnelius <terergurnelius@gmail.com>
2024-01-09 23:25:41 +0300

    Initial commit

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+'''
+    Inserts a line of text to a file, after each
+    line containing a specific string.
+'''
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """
+    Inserts a line of text after each line containing
+    a specific string in a file.
+
+    Parameters:
+        filename (str): The name or path of the file.
+        search_string (str): The specific string to
+            search for in each line.
+        new_string (str): The line of text to insert after
+            each line containing the search string.
+
+    Returns:
+        None
+    """
+    with open(filename, 'r+') as file:
+        lines = file.readlines()
+        file.seek(0)
+
+        for line in lines:
+            file.write(line)
+            if search_string in line:
+                file.write(new_string + '\n')
