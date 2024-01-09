abc92a0cbcfad9cc4a710c109136e6516340fac0
Gurnelius <terergurnelius@gmail.com>
2024-01-09 12:57:52 +0300

    Initial commit

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''
+    Create object from JSON file
+'''
+
+
+import json
+
+def load_from_json_file(filename):
+    """
+    Loads data from a JSON file and returns the
+    corresponding Python object.
+
+    Parameters:
+        filename (str): The name or path of the
+        JSON file to be loaded.
+    Returns:
+        Any: A Python object representing the data stored
+        in the JSON file.
+    """
+    with open(filename, 'r') as file:
+        var = json.loads(file)
+    return var
