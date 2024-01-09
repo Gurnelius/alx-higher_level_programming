70555136fa08cdaad678ea52a8946990a3825f00
Gurnelius <terergurnelius@gmail.com>
2024-01-09 12:49:21 +0300

    Initial commit

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -8,4 +8,14 @@ import json
 
 
 def to_json_string(my_obj):
+    """
+    Converts a Python object to a JSON-formatted string.
+
+    Parameters:
+        my_obj: The Python object to be converted to JSON.
+
+    Returns:
+        str: A JSON-formatted string representing
+        the input object.
+    """
     return json.dumps(my_obj)
