eb7d2946c3448af42026d5b44063c16949f5fd0e
Gurnelius <terergurnelius@gmail.com>
2024-01-09 12:51:02 +0300

    Initial commit

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -8,4 +8,15 @@ import json
 
 
 def from_json_string(my_str):
+    """
+    Converts a JSON-formatted string to a Python object.
+
+    Parameters:
+        my_str (str): A JSON-formatted string to be
+        converted to a Python object.
+
+    Returns:
+        Any: A Python object representing the data parsed
+        from the input JSON string.
+    """
     return json.loads(my_str)
