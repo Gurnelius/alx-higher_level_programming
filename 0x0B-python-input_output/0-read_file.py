c2bb3df10c6a5c714164130f2ad45a11418bc225
Gurnelius <terergurnelius@gmail.com>
2024-01-09 12:43:17 +0300

    Initial commit

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,5 +5,16 @@
 
 
 def read_file(filename=""):
+    """
+    Reads the contents of a text file and prints
+    it to the console.
+
+    Parameters:
+        filename (str): The name or path of the
+        text file to be read. Default is an empty string.
+
+    Returns:
+        None
+    """
     with open(filename, 'r', encoding='utf-8') as file:
         print(file.read(), end='')
--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,5 +5,18 @@
 
 
 def write_file(filename="", text=""):
+    """
+    Writes the specified text to a text file.
+
+    Parameters:
+        - filename (str): The name or path of the
+            text file to be written. Default is an
+            empty string.
+        - text (str): The text content to be written
+            to the file. Default is an empty string.
+
+    Returns:
+        int: The number of characters written to the file.
+`   """
     with open(filename, 'w', encoding='utf-8') as file:
         return file.write(text)
