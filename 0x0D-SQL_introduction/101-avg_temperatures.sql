78b00325b10c2263a55996cc2dd9f4ebbd220333
Gurnelius <terergurnelius@gmail.com>
2024-02-13 16:36:13 +0300

    Intitial commit

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,7 +1,7 @@
 -- Display average temperatures by city
 -- ordered by temperature descending
 SELECT city, AVG(value) AS avg_temp
-`FROM temperatures
+FROM temperatures
 GROUP BY city
 ORDER BY avg_temp DESC;
 
