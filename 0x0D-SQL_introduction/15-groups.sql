d0510c2b79a110f220d7ca1f90c9ba8615af1176
Gurnelius <terergurnelius@gmail.com>
2024-02-13 16:05:38 +0300

    Intitial commit

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,6 +1,6 @@
 -- List number of records with same score
-SELECT score, COUNT(score) as "number"
+SELECT score, COUNT(score) as number
 FROM second_table
 GROUP BY score
-ORDER BY "number" DESC;
+ORDER BY number DESC;
 
