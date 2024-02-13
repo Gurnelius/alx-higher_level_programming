9412943536603e36dc9ae335a8f3032994ca5058
Gurnelius <terergurnelius@gmail.com>
2024-02-13 16:38:21 +0300

    Intitial commit

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,8 @@
+-- Display top 3 temperatures by city
+-- ordered by temperature descending
+SELECT city, AVG(value) AS avg_temp
+FROM temperatures
+GROUP BY city
+ORDER BY avg_temp DESC
+LIMIT 3;
+
