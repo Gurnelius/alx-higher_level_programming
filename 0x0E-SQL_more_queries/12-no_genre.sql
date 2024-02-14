2db40e166e91a43ea03745ce549baf2dc6c8a514
Gurnelius <terergurnelius@gmail.com>
2024-02-14 22:12:25 +0300

    Intitial commit

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -5,6 +5,6 @@ SELECT s.title, g.genre_id
 FROM tv_shows AS s
 LEFT JOIN tv_show_genres AS g
 ON g.show_id = s.id
-WHERE s.genre_id IS NULL
+WHERE g.genre_id IS NULL
 ORDER BY s.title ASC, g.genre_id ASC;
 
