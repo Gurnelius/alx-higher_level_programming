c1b33556aae7144d4331cabe155e405720e14c28
Gurnelius <terergurnelius@gmail.com>
2024-03-11 18:58:39 +0300

    Initial commit

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+const arg = process.argv[2];
+const value = parseInt(arg);
+
+if (isNaN(value)) {
+  console.log('Missing number of occurences');
+} else {
+  for (let i = 0; i < value; i++) {
+    console.log('C is fun');
+  }
+}
