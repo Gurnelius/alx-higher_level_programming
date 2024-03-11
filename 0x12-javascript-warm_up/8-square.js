c9a32867b2e6bff5fafb180287e8741a47440433
Gurnelius <terergurnelius@gmail.com>
2024-03-11 19:09:15 +0300

    Initial commit

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+const arg = process.argv[2];
+const value = parseInt(arg);
+let x = '';
+
+if (isNaN(value)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < value; i++) {
+    for (let j = 0; j < value; j++) {
+      x += 'X';
+    }
+    console.log(x);
+    x = '';
+  }
+}
