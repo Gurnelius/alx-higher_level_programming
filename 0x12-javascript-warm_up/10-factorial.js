57651a2d9cfe77bed6b14d6b8a77bb6589f8fcf0
Gurnelius <terergurnelius@gmail.com>
2024-03-11 19:27:31 +0300

    Initial commit

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,19 @@
 #!/usr/bin/node
+
+const arg = process.argv[2];
+const value = parseInt(arg);
+
+function factorial (n) {
+  if (isNaN(n)) {
+    return 1;
+  }
+  if (n === 0) {
+    return 0;
+  }
+  if (n === 1) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+
+console.log(factorial(value));
