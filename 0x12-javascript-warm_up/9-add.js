14a6b774429485b1fc96ab10a209c3ae616a9d32
Gurnelius <terergurnelius@gmail.com>
2024-03-11 19:18:48 +0300

    Initial commit

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,1 +1,17 @@
 #!/usr/bin/node
+
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
+
+function add (a, b) {
+  const c = parseInt(a);
+  const d = parseInt(b);
+
+  if (isNaN(c) || isNaN(d)) {
+    console.log('NaN');
+  } else {
+    console.log(c + d);
+  }
+}
+
+add(arg1, arg2);
