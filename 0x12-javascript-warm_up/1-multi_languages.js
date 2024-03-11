f4fab06d96af2fb9a7607f262d4a7f6a3dd29e06
Gurnelius <terergurnelius@gmail.com>
2024-03-11 17:59:03 +0300

    Initial commit

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-console.log("C is fun");
-console.log("JavaScript is cool");
-console.log("JavaScript is amazing");
+console.log('C is fun');
+console.log('JavaScript is cool');
+console.log('JavaScript is amazing');
