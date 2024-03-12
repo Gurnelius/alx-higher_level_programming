9db66627a7285715d87093cc8e21ccfbcab3fd97
Gurnelius <terergurnelius@gmail.com>
2024-03-12 21:29:00 +0300

    Initial commit

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,35 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+      return this;
+    }
+    this.width = w;
+    this.height = h;
+  }
+
+  print () {
+    let x = '';
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        x += 'X';
+      }
+      console.log(x);
+      x = '';
+    }
+  }
+
+  rotate () {
+    const temp = this.height;
+
+    this.height = this.width;
+    this.width = temp;
+  }
+
+  double () {
+    this.height = this.height * 2;
+    this.width = this.width * 2;
+  }
+}
+
+module.exports = Rectangle;
