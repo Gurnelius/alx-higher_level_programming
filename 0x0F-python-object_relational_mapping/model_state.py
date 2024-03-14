956e25ce9d2e82e90118368045abb54fbcbd9fd5
Gurnelius <terergurnelius@gmail.com>
2024-03-14 21:29:43 +0300

    Initial commit

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class State(Base):
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+
+if __name__ == "__main__":
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+
+    # Define the connection string
+    db_username = 'root'
+    db_password = 'Pymaster'
+    db_host = 'localhost'
+    db_port = '3306'
+    db_name = 'hbtn_0e_6_usa'
+
+    # Create engine
+    engine = create_engine(f'mysql://{db_username}:{db_password}@{db_host}:{db_port}/{db_name}')
+
+    # Create tables
+    Base.metadata.create_all(engine)
+
+    # Create session
+    Session = sessionmaker(bind=engine)
+    session = Session()
