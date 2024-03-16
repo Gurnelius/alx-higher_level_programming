0fdc34b20f6f24de2d3d038df2add88e3c667c42
Gurnelius <terergurnelius@gmail.com>
2024-03-16 11:50:17 +0300

    Initial commit

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+'''
+    Write a script that lists all cities from the database hbtn_0e_4_usa
+'''
+
+
 import sys
 import MySQLdb
+
 
 if __name__ == "__main__":
     username = sys.argv[1]
