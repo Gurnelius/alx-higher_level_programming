72a6ed556413bb6e65a510068d7d22401a03977d
Gurnelius <terergurnelius@gmail.com>
2024-03-28 19:18:31 +0300

    Initial commit

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -11,6 +11,5 @@ import sys
 url = sys.argv[1]
 
 with urllib.request.urlopen(url) as response:
-    value = response.headers['X-Request-Id']
-
-print(value)
+    x_request_id = response.getheader('X-Request-Id')
+    print(x_request_id)
