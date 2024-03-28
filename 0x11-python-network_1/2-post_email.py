4a2ea8ccd72838a220d24f898542c86838a6b01b
Gurnelius <terergurnelius@gmail.com>
2024-03-28 19:29:34 +0300

    Initial commit

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -10,12 +10,13 @@ import urllib.parse
 import sys
 
 
-url = sys.argv[1]
-email = sys.argv[2]
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
 
-data = urllib.parse.urlencode({'email': email}).encode('utf-8')
-req = urllib.request.Request(url, data=data, method='POST')
+    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+    req = urllib.request.Request(url, data=data, method='POST')
 
-with urllib.request.urlopen(req) as response:
-    body = response.read().decode('utf-8')
-    print("Your email is:", body)
+    with urllib.request.urlopen(req) as response:
+        body = response.read().decode('utf-8')
+        print("Your email is:", body.get('email'))
